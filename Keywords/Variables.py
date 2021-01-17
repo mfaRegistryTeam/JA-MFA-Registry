13e9658f8b1ba18fc248581b299b585be4265725
joshua-alkins <55319780+joshua-alkins@users.noreply.github.com>
2021-01-17 21:07:32 +0800

    changed db uri information

--- Keywords/Variables.py
+++ Keywords/Variables.py
@@ -2,11 +2,12 @@
 
 class siteLabels:
    
-    # DatabaseName = "Citizens-Database-JM"
+    DatabaseName = "Citizens-Database-JM"
     # DatabaseURL = "mongodb://localhost:27017/Citizens-Database-JM"
     SessionType = "mongodb"
-    DatabaseName = "heroku_qc5l7qqd"
-    DatabaseURL = "mongodb://heroku_qc5l7qqd:or7uuplla29cvq7u647oo7ooap@ds163905.mlab.com:63905/heroku_qc5l7qqd?retryWrites=false"
+    # DatabaseName = "heroku_qc5l7qqd"
+    # DatabaseURL = "mongodb://heroku_qc5l7qqd:or7uuplla29cvq7u647oo7ooap@ds163905.mlab.com:63905/heroku_qc5l7qqd?retryWrites=false"
+    DatabaseURL = "mongodb+srv://JoshuaAlkins:xdYDFnJ2CsppXSmM@cluster0.ogus1.mongodb.net/"+DatabaseName+"?retryWrites=true&w=majority"
 
 
 class correspondence:
